bbf3dfeb36f39e96c59b68a55a6fbca2fdd08966
jniemeyer46 <jjniemeyer46@gmail.com>
2017-09-12 01:56:55 -0500

    working on the output into the txt files, also got the time seed in the config file to work

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import random
 import time
 import Length
 
-seed = time.time()
-print(seed)
-random.seed(seed)
 
-print(random.random())
+
 def main():
+	#obtain configs in a list format
+	config = open(sys.argv[1]).read().splitlines()
+
 
 	# obtain the problem file and throw it into a list object
 	shapes = open(sys.argv[2]).read().splitlines()
-	config = open(sys.argv[1]).read().splitlines()
 	
+	# setting up variables using config file
 	for rules in config:
 		info = rules.split(" ")
 		if info[0] == "fitness_evaluations":
@@ -25,21 +25,38 @@ def main():
 			prob_log_file = info[1]
 		elif info[0] == "prob_solution":
 			prob_solution_file = info[1]
-		else:
-			pass
+		elif info[0] == "seed":
+			seed = eval(info[1])
 
-	print(evaluations + " " + num_runs + " " + prob_log_file + " " + prob_solution_file)
+	# Seeds the random function using a saved value that is put into the log file
+	random.seed(seed)
 
 	# Variables that will be used to set the 2d array of material
 	maxWidth = shapes[0].split(" ",1)[0]
 	maxLength = Length.getLength(shapes)
 
+	# opening the log file 
+	result_log = open(prob_log_file, 'w')
+	# formatting the result log with Result Log at the top
+	result_log.write("Result Log\n")
+	result_log.write("Random Seed = " + str(seed) + "\n\n")
+
 	# runs through the program as many times as the config files says to
-	for run in range(0, int(num_runs)):
+	for run in range(1, int(num_runs) + 1):
+		# Titles each section with Run i, where i is the run number (1-30)
+		result_log.write("Run " + str(run) + "\n")
 
 		# the material sheet being used to cut out shapes
 		materialSheet = [[0 for x in range(0, int(maxWidth))] for y in range(0, int(maxLength))]
 
+		for fitness in range(0, int(evaluations)):
+			solution = []
+			result_log.write(str(fitness + 1 ) + "	" + "score\n")
+
+		# formatting the result log with a space after each run block
+		result_log.write("\n")
+
+	result_log.close()
 
 if __name__ == '__main__':
 	main()
