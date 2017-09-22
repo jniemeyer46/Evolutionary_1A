6584d26d06d204309eb5a6da1724bac1277e9a4d
jniemeyer46 <jjniemeyer46@gmail.com>
2017-09-22 14:21:03 -0500

    Updated a lot of files, mostly fixing mistakes made in part 1a.
    Also went ahead and set a new class up to hold all of the information, and I got the program ready to start the EA algorithm

---
+++ Container.py
@@ -0,0 +1,14 @@
+class Container:
+	maxWidth = 0
+	maxlength = 0
+
+	shapes = []
+	numShapes = 0
+
+	materialSheet = []
+
+	evaluations = 0
+	numRuns = 0
+	prob_log_file = 0
+	prob_solution_file = 0
+	seed = 0
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from Container import Container
+from copy import deepcopy
 import sys
 import string
 import random
@@ -8,129 +10,154 @@ import shapeManipulation
 
 
 def main():
+	container = Container()
+
 	#obtain configs in a list format
 	config = open(sys.argv[1]).read().splitlines()
 
 	# obtain the problem file and throw it into a list object
-	shapes = open(sys.argv[2]).read().splitlines()
-	
-	# setting up variables using config file
-	for rules in config:
-		info = rules.split(" ")
-		if info[0] == "fitness_evaluations":
-			evaluations = info[1]
-		elif info[0] == "runs":
-			num_runs = info[1]
-		elif info[0] == "prob_log":
-			prob_log_file = info[1]
-		elif info[0] == "prob_solution":
-			prob_solution_file = info[1]
-		elif info[0] == "seed":
-			seed = eval(info[1])
-
-	# Seeds the random function using a saved value that is put into the log file
-	random.seed(seed)
+	container.shapes = open(sys.argv[2]).read().splitlines()
 
 	# Variables that will be used to set the 2d array of material
-	maxWidth = shapes[0].split(" ")[0]
-	maxLength = Length.getLength(shapes)
+	container.maxWidth = container.shapes[0].split(" ")[0]
+	container.maxLength = Length.getLength(container.shapes)
 	#number of shapes in the problem file
-	numShapes = shapes[0].split(" ")[1]
+	container.numShapes = container.shapes[0].split(" ")[1]
 
 	# delete the width and number of shapes from the shape list
-	del shapes[0]
+	del container.shapes[0]
 
-	# opening the log file 
-	result_log = open(prob_log_file, 'w')
-	# formatting the result log with Result Log at the top
-	result_log.write("Result Log \n")
-	result_log.write("Problem Instance Path = ../%s \n" % sys.argv[2])
-	result_log.write("Random Seed = %s \n" % seed)
-	result_log.write("Parameters used = {'fitness evaluations': %s, 'number of runs': %s, 'problem solution location': '%s' \n\n}" % (evaluations, num_runs, prob_solution_file))
+	# User decided on the Randomized run
+	if sys.argv[3] == "Random":
 
-	# runs through the program as many times as the config files says to
-	for run in range(1, int(num_runs) + 1):
-		# highest fitness calculation thus far this run
-		highest_fitness = 0
+		# setting up variables using config file
+		if sys.argv[4] == "newSeed":
+			for rules in config:
+				info = rules.split(" ")
+				if info[0] == "fitness_evaluations":
+					container.evaluations = info[1]
+				elif info[0] == "runs":
+					container.numRuns = info[1]
+				elif info[0] == "prob_log":
+					container.prob_log_file = info[1]
+				elif info[0] == "prob_solution":
+					container.prob_solution_file = info[1]
+				elif info[0] == "seed":
+					container.seed = eval(info[1])
+		elif sys.argv[4] == "lastSeed":
+			for rules in config:
+				info = rules.split(" ")
+				if info[0] == "fitness_evaluations":
+					container.evaluations = info[1]
+				elif info[0] == "runs":
+					container.numRuns = info[1]
+				elif info[0] == "prob_log":
+					container.prob_log_file = info[1]
+				elif info[0] == "prob_solution":
+					container.prob_solution_file = info[1]
+				elif info[0] == "seed":
+					obtain_seed = open(container.prob_log_file).read().splitlines(3)
+					for lines in obtain_seed:
+						line = lines.split(" ")
+						if line[0] == "Random":
+							container.seed = line[3]
+							break
 
-		# Open the current solution file to obtain the fitness value
-		solution_file = open(prob_solution_file).read().splitlines()
-			
-		# grabs the solution file's fitness value
-		solution_fitness = solution_file[1].split(" ")[3]
+		# Seeds the random function using a saved value that is put into the log file
+		random.seed(container.seed)
 
-		# Titles each section with Run i, where i is the run number (1-30)
-		result_log.write("Run " + str(run) + "\n")
+		# opening the log file 
+		result_log = open(container.prob_log_file, 'w')
+		# formatting the result log with Result Log at the top
+		result_log.write("Result Log \n")
+		result_log.write("Problem Instance Path = ../%s \n" % sys.argv[2])
+		result_log.write("Random Seed = %s \n" % container.seed)
+		result_log.write("Parameters used = {'fitness evaluations': %s, 'number of runs': %s, 'problem solution location': '%s'}\n\n"
+						% (container.evaluations, container.numRuns, container.prob_solution_file))
 
-		# run through the given amount of times given by fitness evaluation
-		for fitness in range(1, int(evaluations)+1):
-			# list of solution locations incase it is the best
-			solution_locations = []
+		# runs through the program as many times as the config files says to
+		for run in range(1, int(container.numRuns) + 1):
+			# highest fitness calculation thus far this run
+			highest_fitness = 0
 
-			# holders for length of material used
-			LargestX = 0
-			SmallestX = 156
+			# Open the current solution file to obtain the fitness value
+			solution_file = open(container.prob_solution_file).read().splitlines()
+				
+			# grabs the solution file's fitness value
+			solution_fitness = solution_file[1].split(" ")[3]
 
-			# the material sheet being used to cut out shapes
-			materialSheet = [[0 for x in range(0, int(maxWidth))] for y in range(0, int(maxLength))]
+			# Titles each section with Run i, where i is the run number (1-30)
+			result_log.write("Run " + str(run) + "\n")
 
-			# for every shape in the file, choose a position
-			for shape in shapes:
-				if not shape[0].isdigit():
-					valid = False
+			# run through the given amount of times given by fitness evaluation
+			for fitness in range(1, int(container.evaluations)+1):
+				# list of solution locations incase it is the best
+				solution_locations = []
 
-					# Keep obtaining a new position until it fits on the material
-					while not valid:
-						# generate random position and rotation
-						x_cord = random.randrange(0, int(maxLength))
-						y_cord = random.randrange(0, int(maxWidth))
-						rotation = random.randrange(0,4)
+				# holders for length of material used
+				LargestX = 0
+				SmallestX = 156
 
-						# Rotate the shape if needed
-						if rotation != 0:
-							shape = rotate.rotate_shape(rotation, shape)
+				# the material sheet being used to cut out shapes
+				container.materialSheet = [[0 for x in range(0, int(container.maxWidth))] for y in range(0, int(container.maxLength))]
 
-						# Check whether the shape fits on the material in the current position
-						valid = shapeManipulation.validPlacement(materialSheet, maxLength, maxWidth, x_cord, y_cord, shape)
-							
-						# if the move was valid and was placed
-						if valid:
-							shapeManipulation.placeShape(materialSheet, x_cord, y_cord, shape)
-							# store the location in a tuple if it worked
-							placementLocation = (x_cord, y_cord, rotation)
-							# append it to the list
-							solution_locations.append(placementLocation)
+				# for every shape in the file, choose a position
+				for shape in container.shapes:
+					if not shape[0].isdigit():
+						valid = False
 
-			# obtains the smallest and largest position in the material array
-			for i in range(len(materialSheet)):
-				if 1 in materialSheet[i]:
-					if i < SmallestX:
-						SmallestX = i
-					elif i > LargestX:
-						LargestX = i
+						# Keep obtaining a new position until it fits on the material
+						while not valid:
+							# generate random position and rotation
+							x_cord = random.randrange(0, int(container.maxLength))
+							y_cord = random.randrange(0, int(container.maxWidth))
+							rotation = random.randrange(0,4)
 
-			# Determines the Length of the material used by this iteration
-			usedLength = ((LargestX - SmallestX) + 1)
-			#print(usedLength)
-			current_fitness = fitnessCalc(maxLength, usedLength)
-			if highest_fitness < current_fitness:
-				highest_fitness = current_fitness
-				result_log.write(str(fitness + 1 ) + "	" + str(current_fitness) + "\n")
+							# Rotate the shape if needed
+							if rotation != 0:
+								shape = rotate.rotate_shape(rotation, shape)
 
-			# If the current solution is the best, replace the info in the file with the new solution
-			if int(solution_fitness) < highest_fitness:
-				solution_file = open(prob_solution_file, 'w')
+							# Check whether the shape fits on the material in the current position
+							valid = shapeManipulation.validPlacement(container.materialSheet, container.maxLength, container.maxWidth, x_cord, y_cord, shape)
+								
+							# if the move was valid and was placed
+							if valid:
+								shapeManipulation.placeShape(container.materialSheet, x_cord, y_cord, shape)
+								# store the location in a tuple if it worked
+								placementLocation = (x_cord, y_cord, rotation)
+								# append it to the list
+								solution_locations.append(placementLocation)
 
-				solution_file.write("Solution File\n")
-				solution_file.write("Fitness Calculation = " + str(highest_fitness) + "\n\n")
+				# obtains the smallest and largest position in the material array
+				for i in range(len(container.materialSheet)):
+					if 1 in container.materialSheet[i]:
+						if i < SmallestX:
+							SmallestX = i
+						elif i > LargestX:
+							LargestX = i
 
-				for i in range(len(solution_locations)):
-					solution_file.write(str(solution_locations[i])[1:-1] + "\n")
+				# Determines the Length of the material used by this iteration
+				usedLength = ((LargestX - SmallestX) + 1)
+				#print(usedLength)
+				current_fitness = fitnessCalc(container.maxLength, usedLength)
+				if highest_fitness < current_fitness:
+					highest_fitness = current_fitness
+					result_log.write(str(fitness + 1 ) + "	" + str(current_fitness) + "\n")
 
-		# formatting the result log with a space after each run block
-		result_log.write("\n")
+				# If the current solution is the best, replace the info in the file with the new solution
+				if int(solution_fitness) < highest_fitness:
+					solution_file = open(prob_solution_file, 'w')
 
-	result_log.close()
+					solution_file.write("Solution File\n")
+					solution_file.write("Fitness Calculation = " + str(highest_fitness) + "\n\n")
+
+					for i in range(len(solution_locations)):
+						solution_file.write(str(solution_locations[i])[1:-1] + "\n")
+
+			# formatting the result log with a space after each run block
+			result_log.write("\n")
+
+		result_log.close()
 
 
 def fitnessCalc(maxLength, usedLength):
@@ -138,7 +165,6 @@ def fitnessCalc(maxLength, usedLength):
 	fitness_calculation = maxLength - usedLength
 
 	return fitness_calculation
-
 
 if __name__ == '__main__':
 	main()
